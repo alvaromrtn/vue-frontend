2f90332e7d4143fa9537a4bda08e1104237f69de
alvaromrtn <alvaromrtn@gmail.com>
2022-03-02 11:10:36 +0100

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Profesor/>
+  </div>
+</template>
+
+<script>
+import Profesor from './components/Profesor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Profesor
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Profesor.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+      <h1 class="text-center">Lista de Profesores</h1>
+      <table class="table table-striped">
+        <thead>
+           <th>ID</th>
+           <th>NOMBRE</th>
+           <th>APELLIDO</th>
+           <th>CORREO</th>
+        </thead>
+        <tbody>
+          <tr v-for="profesor in profesores" v-bind:key="profesor.id">
+            <td>{{profesor.id}}</td>
+            <td>{{profesor.nombre}}</td>
+            <td>{{profesor.apellidos}}</td>
+            <td>{{profesor.correo}}</td>
+          </tr>
+        </tbody>
+       </table>
+    </div>
+</template>
+
+<script>
+
+  import Profesor_Service from "../services/Profesor_Service"
+
+  export default {
+    name: 'ProfesorScript',
+    data() {
+      return {
+        profesores : []
+      }
+    },
+    methods: {
+      getProfesores() {
+        Profesor_Service.getProfesores().then((response) => {
+          this.profesores = response.data;
+        });
+      }
+    },
+    created() {
+      this.getProfesores()
+    }
+  }
+
+</script>
