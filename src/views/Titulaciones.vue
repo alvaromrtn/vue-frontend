78c9841840adf5c3afa8fd1004f9944c19982922
alvaromrtn <alvaromrtn@gmail.com>
2022-05-03 18:57:01 +0200

    GIF de carga mientras se obtienen los datos

--- src/views/Titulaciones.vue
+++ src/views/Titulaciones.vue
@@ -1,28 +1,38 @@
 <template>
   <div class="container">
     <h1 class="text-center">Lista de Titulaciones</h1>
-    <table class="table table-striped">
-      <thead>
-        <th>ID</th>
-        <th>CENTRO</th>
-        <th>CODIGO</th>
-        <th>NOMBRE</th>
-        <th>CURSOS</th>
-      </thead>
-      <tbody>
-        <tr
-          v-for="titulacion in titulaciones"
-          v-bind:key="titulacion.id"
-          v-on:click="navegarTitulacion(titulacion.codigo)"
-        >
-          <td>{{ titulacion.id }}</td>
-          <td>{{ titulacion.centro }}</td>
-          <td>{{ titulacion.codigo }}</td>
-          <td>{{ titulacion.nombre }}</td>
-          <td>{{ titulacion.numeroCursos }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div v-if="datosCargados">
+      <table class="table table-striped">
+        <thead>
+          <th>ID</th>
+          <th>CENTRO</th>
+          <th>CODIGO</th>
+          <th>NOMBRE</th>
+          <th>CURSOS</th>
+        </thead>
+        <tbody>
+          <tr
+            v-for="titulacion in titulaciones"
+            v-bind:key="titulacion.id"
+            v-on:click="navegarTitulacion(titulacion.codigo)"
+          >
+            <td>{{ titulacion.id }}</td>
+            <td>{{ titulacion.centro }}</td>
+            <td>{{ titulacion.codigo }}</td>
+            <td>{{ titulacion.nombre }}</td>
+            <td>{{ titulacion.numeroCursos }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div v-else>
+      <img
+        src="../../static/gifs/carga.gif"
+        class="img-responsive"
+        height="100"
+        width="100"
+      />
+    </div>
   </div>
 </template>
 
@@ -34,12 +44,15 @@ export default {
   data() {
     return {
       titulaciones: [],
+      datosCargados: false,
     };
   },
+  datosCargados: true,
   methods: {
     getTitulaciones() {
       Titulaciones_Service.getTitulaciones().then((response) => {
         this.titulaciones = response.data;
+        this.datosCargados = true;
       });
     },
     navegarTitulacion(codigo) {
