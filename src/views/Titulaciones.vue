5cf766013e5d8e359bf8dad960c923c3e25f29e1
alvaromrtn <alvaromrtn@gmail.com>
2022-05-04 15:24:45 +0200

    Paginación en Titulaciones

--- src/views/Titulaciones.vue
+++ src/views/Titulaciones.vue
@@ -1,7 +1,29 @@
 <template>
   <div class="container">
     <h1 class="text-center">Lista de Titulaciones</h1>
+
+    <h1 class="text-center">TOTAL DE PAGINAS: {{ totalPaginas() }}</h1>
+
     <div v-if="datosCargados">
+      <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+          <li class="page-item" v-on:click="getPreviousPage()">
+            <a class="page-link" href="#">Previous</a>
+          </li>
+          <li
+            v-for="pagina in totalPaginas()"
+            :key="pagina"
+            v-on:click="getDataPagina(pagina)"
+            class="page-item"
+            v-bind:class="isActive(pagina)"
+          >
+            <a class="page-link" href="#">{{ pagina }}</a>
+          </li>
+          <li class="page-item" v-on:click="getNextPage()">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
       <table class="table table-striped">
         <thead>
           <th>ID</th>
@@ -12,7 +34,7 @@
         </thead>
         <tbody>
           <tr
-            v-for="titulacion in titulaciones"
+            v-for="titulacion in titulacionesPaginadas"
             v-bind:key="titulacion.id"
             v-on:click="navegarTitulacion(titulacion.codigo)"
           >
@@ -37,25 +59,69 @@ import ProcesoCarga from "./ProcesoCarga";
 
 export default {
   name: "TitulacionesScript",
+  components: {
+    ProcesoCarga,
+  },
   data() {
     return {
       titulaciones: [],
       datosCargados: false,
+      //
+      elementosPorPagina: 10,
+      titulacionesPaginadas: [],
+      paginaActual: 1,
     };
-  },
-  components: {
-    ProcesoCarga,
   },
   methods: {
     getTitulaciones() {
       Titulaciones_Service.getTitulaciones().then((response) => {
         this.titulaciones = response.data;
         this.datosCargados = true;
+        this.getDataPagina(1);
       });
     },
     navegarTitulacion(codigo) {
       var ruta = "/titulacion/" + codigo;
       window.location.href = ruta;
+    },
+    totalPaginas() {
+      return Math.ceil(this.titulaciones.length / this.elementosPorPagina);
+    },
+    getDataPagina(numeroPagina) {
+      this.paginaActual = numeroPagina;
+      this.titulacionesPaginadas = [];
+      let ini =
+        numeroPagina * this.elementosPorPagina - this.elementosPorPagina;
+      let fin = numeroPagina * this.elementosPorPagina;
+      /*
+      for (let index = ini; index < fin; index++) {
+        this.titulacionesPaginadas.push(this.titulaciones[index]);
+      }
+      */
+      this.titulacionesPaginadas = this.titulaciones.slice(ini, fin);
+    },
+    getPreviousPage() {
+      if (this.paginaActual > 1) {
+        this.paginaActual--;
+      }
+      this.getDataPagina(this.paginaActual);
+    },
+    getNextPage() {
+      if (this.paginaActual < this.totalPaginas()) {
+        this.paginaActual++;
+      }
+
+      this.getDataPagina(this.paginaActual);
+    },
+    isActive(numeroPagina) {
+      //active
+      /*
+      if (numeroPagina == this.paginaActual) {
+        return "active";
+      } else {
+        return "";
+      }*/
+      return numeroPagina == this.paginaActual ? "active" : "";
     },
   },
   created() {
@@ -63,3 +129,21 @@ export default {
   },
 };
 </script>
+
+<style>
+.pagination > li > a {
+  background-color: white;
+  color: black;
+}
+
+.pagination > .active > a {
+  color: white;
+  background-color: red !important;
+  border: solid 1px red !important;
+}
+
+.pagination > .active > a:hover {
+  background-color: red !important;
+  border: solid 1px red;
+}
+</style>
