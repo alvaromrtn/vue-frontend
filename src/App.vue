6975fa6bee83b7de9bb2c4a48cb23eaebacc2035
alvaromrtn <alvaromrtn@gmail.com>
2022-05-11 15:09:13 +0200

    Grid filtro-paginación-ordenación titulaciones y ordenacion ascendente y descendente

--- src/App.vue
+++ src/App.vue
@@ -28,22 +28,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
-
-#nav {
-  padding: 30px;
-  text-align: center;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: whitesmoke;
-  background: crimson;
-  border-radius: 0.5rem;
 }
 </style>
--- src/views/Titulaciones.vue
+++ src/views/Titulaciones.vue
@@ -2,35 +2,67 @@
   <div class="container">
     <h1 class="text-center">Titulaciones</h1>
 
+    <div class="grid">
+      <div id="filtro">
+        <form action="/action_page.php" id="carform">
+          <label for="fname">Firstname:</label>
+          <input type="text" id="fname" name="fname" />
+          <input type="submit" />
+        </form>
+
+        <label for="cars">Choose a car:</label>
+        <select name="cars" id="cars" form="carform">
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="opel">Opel</option>
+          <option value="audi">Audi</option>
+        </select>
+      </div>
+      <div id="paginación">
+        <nav aria-label="PaginaciónTablaTitulaciones">
+          <ul class="pagination justify-content-center">
+            <li class="page-item" v-on:click="getPaginaAnterior()">
+              <a class="page-link">Anterior</a>
+            </li>
+            <li
+              v-for="pagina in numeroTotalPaginas()"
+              :key="pagina"
+              v-on:click="getTitulacionesPagina(pagina)"
+              class="page-item"
+              v-bind:class="paginaActiva(pagina)"
+            >
+              <a class="page-link">{{ pagina }}</a>
+            </li>
+            <li class="page-item" v-on:click="getPaginaSiguiente()">
+              <a class="page-link">Siguiente</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="ordenación">
+        <div class="dropdown" style="float: right">
+          <button class="dropbtn">Ordenar</button>
+          <div class="dropdown-content">
+            <a v-on:click="ordenarPor('centro_descendente')">CENTRO ▾</a>
+            <a v-on:click="ordenarPor('centro_ascendente')">CENTRO ▴</a>
+            <a v-on:click="ordenarPor('codigo_descendente')">CODIGO ▾</a>
+            <a v-on:click="ordenarPor('codigo_ascendente')">CODIGO ▴</a>
+            <a v-on:click="ordenarPor('nombre_descendente')">NOMBRE ▾</a>
+            <a v-on:click="ordenarPor('nombre_ascendente')">NOMBRE ▴</a>
+            <a v-on:click="ordenarPor('numeroCursos_descendente')">CURSOS ▾</a>
+            <a v-on:click="ordenarPor('numeroCursos_ascendente')">CURSOS ▴</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div v-if="datosCargados">
-      <nav aria-label="PaginaciónTablaTitulaciones">
-        <ul class="pagination justify-content-center">
-          <li class="page-item" v-on:click="getPaginaAnterior()">
-            <a class="page-link" href="#">Anterior</a>
-          </li>
-          <li
-            v-for="pagina in numeroTotalPaginas()"
-            :key="pagina"
-            v-on:click="getTitulacionesPagina(pagina)"
-            class="page-item"
-            v-bind:class="paginaActiva(pagina)"
-          >
-            <a class="page-link" href="#">{{ pagina }}</a>
-          </li>
-          <li class="page-item" v-on:click="getPaginaSiguiente()">
-            <a class="page-link" href="#">Siguiente</a>
-          </li>
-        </ul>
-      </nav>
       <table class="table table-striped table-responsive">
         <thead>
-          <th style="width: 5%" v-on:click="ordenarPor('id')">ID</th>
-          <th style="width: 40%" v-on:click="ordenarPor('centro')">CENTRO</th>
-          <th style="width: 10%" v-on:click="ordenarPor('codigo')">CODIGO</th>
-          <th style="width: 40%" v-on:click="ordenarPor('nombre')">NOMBRE</th>
-          <th style="width: 5%" v-on:click="ordenarPor('numeroCursos')">
-            CURSOS
-          </th>
+          <th style="width: 40%">CENTRO</th>
+          <th style="width: 15%">CODIGO</th>
+          <th style="width: 40%">NOMBRE</th>
+          <th style="width: 5%">CURSOS</th>
         </thead>
         <tbody>
           <tr
@@ -38,9 +70,8 @@
             v-bind:key="titulacion.id"
             v-on:click="navegarTitulacion(titulacion.codigo)"
           >
-            <td style="width: 5%">{{ titulacion.id }}</td>
             <td style="width: 40%">{{ titulacion.centro }}</td>
-            <td style="width: 10%">{{ titulacion.codigo }}</td>
+            <td style="width: 15">{{ titulacion.codigo }}</td>
             <td style="width: 40%">{{ titulacion.nombre }}</td>
             <td style="width: 5%">{{ titulacion.numeroCursos }}</td>
           </tr>
@@ -70,6 +101,7 @@ export default {
       titulacionesPaginadas: [],
       elementosPorPagina: 10,
       paginaActual: 1,
+      //
     };
   },
   methods: {
@@ -112,17 +144,7 @@ export default {
     },
     ordenarPor(campo) {
       switch (campo) {
-        case "id":
-          this.titulaciones.sort((a, b) => {
-            if (a.id < b.id) {
-              return -1;
-            }
-            if (a.id > b.id) {
-              return 1;
-            }
-          });
-          break;
-        case "centro":
+        case "centro_descendente":
           this.titulaciones.sort((a, b) => {
             if (a.centro < b.centro) {
               return -1;
@@ -132,7 +154,7 @@ export default {
             }
           });
           break;
-        case "codigo":
+        case "codigo_descendente":
           this.titulaciones.sort((a, b) => {
             if (a.codigo < b.codigo) {
               return -1;
@@ -142,7 +164,7 @@ export default {
             }
           });
           break;
-        case "nombre":
+        case "nombre_descendente":
           this.titulaciones.sort((a, b) => {
             if (a.nombre < b.nombre) {
               return -1;
@@ -152,13 +174,53 @@ export default {
             }
           });
           break;
-        case "numeroCursos":
+        case "numeroCursos_descendente":
           this.titulaciones.sort((a, b) => {
             if (a.numeroCursos < b.numeroCursos) {
               return -1;
             }
             if (a.numeroCursos > b.numeroCursos) {
               return 1;
+            }
+          });
+          break;
+        case "centro_ascendente":
+          this.titulaciones.sort((a, b) => {
+            if (a.centro < b.centro) {
+              return 1;
+            }
+            if (a.centro > b.centro) {
+              return -1;
+            }
+          });
+          break;
+        case "codigo_ascendente":
+          this.titulaciones.sort((a, b) => {
+            if (a.codigo < b.codigo) {
+              return 1;
+            }
+            if (a.codigo > b.codigo) {
+              return -1;
+            }
+          });
+          break;
+        case "nombre_ascendente":
+          this.titulaciones.sort((a, b) => {
+            if (a.nombre < b.nombre) {
+              return 1;
+            }
+            if (a.nombre > b.nombre) {
+              return -1;
+            }
+          });
+          break;
+        case "numeroCursos_ascendente":
+          this.titulaciones.sort((a, b) => {
+            if (a.numeroCursos < b.numeroCursos) {
+              return 1;
+            }
+            if (a.numeroCursos > b.numeroCursos) {
+              return -1;
             }
           });
           break;
@@ -173,6 +235,15 @@ export default {
 </script>
 
 <style>
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+
+.pagination {
+  cursor: pointer;
+  padding: 10px;
+}
 .pagination > li > a {
   background-color: white;
   color: black;
@@ -190,9 +261,52 @@ export default {
 }
 
 .table > thead > th {
-  cursor: pointer;
+  cursor: default;
 }
 .table > tbody > tr {
   cursor: pointer;
+}
+
+.dropbtn {
+  background-color: white;
+  color: black;
+  padding: 5px;
+  cursor: pointer;
+  border: solid 1px black;
+  border-radius: 5px;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  right: 0;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {
+  background-color: #f1f1f1;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+.dropdown:hover .dropbtn {
+  background-color: red;
 }
 </style>
