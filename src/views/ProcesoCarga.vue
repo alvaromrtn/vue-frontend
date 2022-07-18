3ee12208b477ab6bd94aeb1ff64e43a4478ab292
alvaromrtn <alvaromrtn@gmail.com>
2022-07-18 10:02:18 +0200

    ProcesoCarga centrado

--- src/views/ProcesoCarga.vue
+++ src/views/ProcesoCarga.vue
@@ -1,18 +1,28 @@
 <template>
-  <div>
-    <img
-      src="../assets/gifs/carga-1.gif"
-      class="img-responsive"
-      height="100"
-      width="100"
-    />
+  <div class="container">
+    <div class="gif">
+      <img
+        src="../assets/gifs/carga-1.gif"
+        class="img-responsive"
+        height="100"
+        width="100"
+      />
+    </div>
   </div>
 </template>
 
 <style>
-img.center {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
+.container {
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-rows: 1fr;
+  gap: 0px 0px;
+}
+.gif {
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 400px;
 }
 </style>
