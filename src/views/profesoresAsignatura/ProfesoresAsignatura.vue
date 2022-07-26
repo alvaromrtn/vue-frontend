cfbbe0ad0e0090a9b4bb0f8b6891eba2073f005f
alvaromrtn <alvaromrtn@gmail.com>
2022-07-26 18:15:13 +0200

    GraficoProfesoresAsignatura

--- src/views/profesoresAsignatura/ProfesoresAsignatura.vue
+++ src/views/profesoresAsignatura/ProfesoresAsignatura.vue
@@ -14,6 +14,12 @@
           :datosTabla="this.profesores"
         ></TablaProfesoresAsignatura>
         <br />
+        <div v-if="datosGrafico.series.length != 0">
+          <h4>GRÁFICO SEGÚN EL NÚMERO DE HORAS POR PROFESOR</h4>
+          <GraficoProfesoresAsignatura
+            :datos="this.datosGrafico"
+          ></GraficoProfesoresAsignatura>
+        </div>
       </div>
       <div v-else>
         <ProcesoCarga />
@@ -25,6 +31,8 @@
 <script>
 import Profesores_Service from "../../services/Profesores_Service.js";
 import TablaProfesoresAsignatura from "./TablaProfesoresAsignatura.vue";
+import GraficoProfesoresAsignatura from "./GraficoProfesoresAsignatura.vue";
+import DatasetGraficoDonut from "../../components/DatasetGraficoDonut_Component.js";
 import ProcesoCarga from "../ProcesoCarga.vue";
 
 export default {
@@ -32,19 +40,38 @@ export default {
   data() {
     return {
       profesores: [],
-      datosGrafico: {},
       datosCargados: false,
       nombreAsignaturaCargado: false,
+      datosGrafico: {},
     };
   },
   components: {
     TablaProfesoresAsignatura,
+    GraficoProfesoresAsignatura,
     ProcesoCarga,
   },
   methods: {
     getProfesoresAsignatura(id) {
       Profesores_Service.getProfesoresAsignatura(id).then((response) => {
         this.profesores = response.data;
+
+        this.profesores.sort((a, b) => {
+          if (a.horas < b.horas) return 1;
+
+          if (a.horas > b.horas) return -1;
+        });
+
+        this.datosGrafico = DatasetGraficoDonut.getDataset();
+
+        let listaProfesores = [];
+        let listaHoras = [];
+        for (let i = 0; i < this.profesores.length; i++) {
+          listaProfesores[i] = this.profesores[i].nombre;
+          listaHoras[i] = this.profesores[i].horas;
+        }
+
+        this.datosGrafico.options.labels = listaProfesores;
+        this.datosGrafico.series = listaHoras;
 
         this.datosCargados = true;
       });
